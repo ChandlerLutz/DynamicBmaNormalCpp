104d99698494fc0516a58577ddaefef02038d393
Chandler Lutz <cl.eco@cbs.dk>
2017-02-21 11:37:11 +0100

    first commit

---
+++ R/DynamicBmaNormalCpp_main.R
@@ -0,0 +1,17 @@
+## c:/Dropbox/Rpackages/DynamicBmaNormalCpp/R/DynamicBmaNormalCpp_main.R
+
+##    Chandler Lutz
+##    Questions/comments: cl.eco@cbs.dk
+##    $Revisions:      1.0.0     $Date:  2017-02-21
+
+#' Dynamic Bayesian Model Averaging with C++
+#'
+#' DynamicBmaNormalCpp implements the Dynamic Bayesian Model technique
+#' of Rafterey et al. (2005) for the normal distribution using C++
+"_PACKAGE"
+
+
+#' @useDynLib DynamicBmaNormalCpp
+#' @importFrom Rcpp evalCpp
+#' @exportPattern "^[[:alpha:]]+"
+NULL
